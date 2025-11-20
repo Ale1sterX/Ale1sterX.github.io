d00c6a0820a9bbeecea7f84ed77782e98dd09eb9
Ale1sterX <90473500+Ale1sterX@users.noreply.github.com>
2025-11-20 14:25:34 +0700

    updated

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// Theme toggle handler
+// THEME TOGGLE
 const toggleBtn = document.getElementById("theme-toggle");
 const body = document.body;
 
-// Load last theme
-if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark");
+if (localStorage.getItem("theme") === "light") {
+    body.classList.add("light");
     toggleBtn.textContent = "☀️";
 }
 
-// Toggle theme on click
 toggleBtn.addEventListener("click", () => {
-    body.classList.toggle("dark");
+    body.classList.toggle("light");
 
-    if (body.classList.contains("dark")) {
-        toggleBtn.textContent = "☀️";
-        localStorage.setItem("theme", "dark");
-    } else {
-        toggleBtn.textContent = "🌙";
+    if (body.classList.contains("light")) {
         localStorage.setItem("theme", "light");
+        toggleBtn.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "dark");
+        toggleBtn.textContent = "🌙";
     }
 });
